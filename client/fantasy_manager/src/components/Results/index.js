c583805e33fe2d6aba415926ee7ac83d3e6788dc
NekoCarreraDesigns <denvermaas@gmail.com>
2020-09-26 22:25:58 -0600

    added results component, and styled it as well

---
+++ client/fantasy_manager/src/components/Results/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./index.css"
+
+function Results(props) {
+
+    return (
+        <div className="search-results">
+            <div className="search-table">
+                <tbody>
+                    <tr className="players">Players</tr>
+                    <td>{props.name}</td>
+                    <tr className="points">Points</tr>
+                    <td>{props.points}</td>
+                </tbody>
+            </div>
+        </div>
+    )
+}
+export default Results
